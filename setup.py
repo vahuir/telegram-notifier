c3a936a859ada62717f9a16394fb5da0fd3d7dc5
Vicent Ahuir Esteve <viahes@dsic.upv.es>
2024-04-07 15:18:16 +0200

    Build correction

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     version="1.0.0",
     name="telegram-notifier",
     description="Telegram Notifier executes bash commands and monitors them.",
     author="Vicent Ahuir",
-    entry_points={"console_scripts": ["telegram-notifier = telegram_notifier:app"]},
+    packages=find_packages(),
+    entry_points={"console_scripts": ["telegram-notifier = telegram_notifier.cli:app"]},
     install_requires=["python-telegram-bot>=21.0.1"],
     python_requires=">=3.8.0",
 )
